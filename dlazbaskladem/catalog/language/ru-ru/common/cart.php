eca41f47cb140bd317a3c6295fadf95885cbe653
7787229 <7787229@gmail.com>
2019-11-08 17:38:55 +0300

    Начало разработки

--- dlazbaskladem/catalog/language/ru-ru/common/cart.php
+++ dlazbaskladem/catalog/language/ru-ru/common/cart.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_items']     = 'Товаров %s (%s)';
+$_['text_items']     = '<span class="txt_number">%s</span><span class="txt_items">Cart</span><span class="total-price">%s</span>';
 $_['text_empty']     = 'Ваша корзина пуста!';
 $_['text_cart']      = 'Перейти в корзину';
 $_['text_checkout']  = 'Оформить заказ';
